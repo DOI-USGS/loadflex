57909d48e6b10b94dbd7b543dc7de3d7e8e1b153
Alison Appling <alison.appling@gmail.com>
2016-01-22 18:13:09 -0600

    initial test suite

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,2 +1,15 @@
 library(testthat)
-#test_check('loadflex')
+library(loadflex)
+
+test_check('loadflex', filter='01|02|11|21|31|32|42')
+# 03 breaks
+# 04 takes forever & has interaction
+# 05 has interaction
+# 06 has interaction
+# 22 has interaction
+# 23 has interaction
+# 24 has interaction
+# 25 breaks
+# 33 has interaction
+# 41 has interaction
+# 44 is empty
