691a8d15f49178c836cea7fafac3234c33932a2a
Miguel Leon <leonmi@sas.upenn.edu>
2016-01-27 15:28:11 -0500

    build error

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,3 @@
 library(testthat)
-#test_check('loadflex', filter='01|02|05|06|11|21|22||23|24|31|32|33||41|42') # travis build failed, rebuild with trivial change
-test_check('loadflex', filter='01|02|05|06|11|21|22|23|24|31|32|41|42')
+#test_check('loadflex', filter='01|02|05|06|11|21|22||23|24|31|32|33||41|42') 
+test_check('loadflex', filter='01|02|05|06|11|21|22|23|24|31|32|41|42')# travis build failed, rebuild with trivial change
