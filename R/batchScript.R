138514c301f9d26273fdd6587d796488cbbdf9d2
wdwatkins <wwatkins@usgs.gov>
2016-12-27 17:00:11 -0500

    test data ready

---
+++ R/batchScript.R
@@ -0,0 +1,18 @@
+#script to run loadflex over multiple sites
+
+#TODO: read-in function
+load('data/ana_test.Rdata')
+#one function/format for now
+
+#loop over unique sites
+for(site in unique(nutriDF$id)){
+  #TODO: metrics
+  
+  #TODO: run models
+  
+  #TODO: model metrics?
+  
+  #TODO: plots
+  
+}
+
