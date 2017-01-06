c6d18da4f31e0e92962342e52383f2965b927674
Lindsay Carr <lcarr@usgs.gov>
2017-01-06 12:42:51 -0600

    update plotEGRET functions available now that convertToEGRET creates correct data needed for more plots

--- R/plotEGRET.R
+++ R/plotEGRET.R
@@ -2,9 +2,8 @@
 #'  
 #' Use a loadflex object, but create an EGRET plot.
 #' 
-#' @param plot.name the name of the plot the user wants to create. Current
-#' options: ConcTime, ConcQ, FluxTimeDaily, and FluxQ. Each require different
-#' data, see Details.
+#' @param plot.name the name of the plot the user wants to create. See 
+#' Details for current options. For now, only one allowed at time.
 #' @param intdat data.frame of interpolation data
 #' @param estdat data.frame of estimation data
 #' @param preds data.frame of load predictions
@@ -15,8 +14,29 @@
 #' concentrations ("Conc") or fluxes ("Flux").
 #' @param ... additional arguments to pass to the plot
 #'
-#' @details ConcTime, ConcQ, and FluxQ require \code{intdat, meta}. FluxTimeDaily
-#' requires \code{intdat, estdat, preds, meta}.
+#' @details EGRET plots that require \code{intdat, meta}:
+#' \itemize{
+#'   \item boxConcMonth
+#'   \item plotConcTime
+#'   \item plotConcQ
+#'   \item plotFluxQ
+#' }
+#' EGRET plots that require \code{intdat, estdat, preds, meta}:
+#' \itemize{
+#'   \item boxQTwice
+#'   \item multiPlotDataOverview
+#'   \item plotConcTimeDaily
+#'   \item plotFluxTimeDaily
+#'   \item plotConcPred 
+#'   \item plotFluxPred 
+#'   \item plotResidPred
+#'   \item plotResidQ  
+#'   \item plotResidTime
+#'   \item boxResidMonth
+#'   \item boxConcThree 
+#'   \item plotConcHist 
+#'   \item plotFluxHist 
+#' }
 #'
 #' @importFrom EGRET plotConcTime
 #' @importFrom EGRET plotConcQ
@@ -28,11 +48,30 @@ plotEGRET <- function(plot.name, intdat = NULL, estdat = NULL, preds = NULL,
                       meta = NULL, preds.type = "Conc", ...) {
   
   req_missing <- switch(plot.name,
-                        ConcTime = missing(intdat) | missing(meta),
-                        ConcQ = missing(intdat) | missing(meta),
-                        FluxTimeDaily = missing(intdat) | missing(meta) |
+                        
+                        # require intdat & meta
+                        boxConcMonth = ,
+                        plotConcTime = ,
+                        plotConcQ = ,
+                        plotFluxQ = missing(intdat) | missing(meta),
+                        
+                        # require intdat, meta, estdat, and preds
+                        boxQTwice = ,
+                        multiPlotDataOverview = ,
+                        plotConcTimeDaily = ,
+                        plotFluxTimeDaily = ,
+                        plotConcPred = , 
+                        plotFluxPred = , 
+                        plotResidPred = ,
+                        plotResidQ = ,  
+                        plotResidTime = ,
+                        boxResidMonth = ,
+                        boxConcThree = , 
+                        plotConcHist = , 
+                        plotFluxHist = missing(intdat) | missing(meta) |
                           missing(estdat) | missing(preds),
-                        FluxQ = missing(intdat) | missing(meta),
+                        
+                        # default if no name matches
                         FALSE)
   
   if(req_missing) {
@@ -42,10 +81,29 @@ plotEGRET <- function(plot.name, intdat = NULL, estdat = NULL, preds = NULL,
   egretobj <- convertToEGRET(intdat, estdat, preds, meta, preds.type)
   
   switch(plot.name,
-         ConcTime = plotConcTime(egretobj, ...),
-         ConcQ = plotConcQ(egretobj, ...),
-         FluxTimeDaily = plotFluxTimeDaily(egretobj, ...),
-         FluxQ = plotFluxQ(egretobj, ...),
+         
+         # require intdat & meta
+         boxConcMonth = boxConcMonth(egretobj, ...),
+         plotConcTime = plotConcTime(egretobj, ...),
+         plotConcQ = plotConcQ(egretobj, ...),
+         plotFluxQ = plotFluxQ(egretobj, ...),
+         
+         # require intdat, meta, estdat, and preds
+         boxQTwice = boxQTwice(egretobj, ...),
+         multiPlotDataOverview = multiPlotDataOverview(egretobj, ...),
+         plotConcTimeDaily = plotConcTimeDaily(egretobj, ...),
+         plotFluxTimeDaily = plotFluxTimeDaily(egretobj, ...),
+         plotConcPred =  plotConcPred(egretobj, ...), 
+         plotFluxPred = plotFluxPred(egretobj, ...), 
+         plotResidPred = plotResidPred(egretobj, ...),
+         plotResidQ =  plotResidQ(egretobj, ...),  
+         plotResidTime = plotResidTime(egretobj, ...),
+         boxResidMonth = boxResidMonth(egretobj, ...),
+         boxConcThree = boxConcThree(egretobj, ...), 
+         plotConcHist =  plotConcHist(egretobj, ...), 
+         plotFluxHist =plotFluxHist(egretobj, ...),
+         
+         # default if no name matches
          stop(paste('unrecognized plot.name:', plot.name)))
   
 }
