7623b4f49429174fd4f28ff39683402aaf5b5eb7
Alison Appling <aappling@usgs.gov>
2017-01-22 21:38:15 -0700

    add as.data.frame.metadata

--- R/metadata.R
+++ R/metadata.R
@@ -677,3 +677,33 @@ setMethod(
     return(TRUE)
   }
 )
+
+#' Convert a metadata object to a 1-row data.frame
+#' 
+#' Organize the fields of a metadata object into a 1-row data.frame. If there is
+#' a custom field, this will
+#' 
+#' @export
+#' @param x a loadflex metadata object
+#' @param row.names NULL or a character vector giving the row names for the data
+#'   frame. Missing values are not allowed.
+#' @param optional logical. If TRUE, setting row names and converting column
+#'   names (to syntactic names: see make.names) is optional. Note that all of
+#'   R's base package as.data.frame() methods use optional only for column names
+#'   treatment, basically with the meaning of data.frame(*, check.names =
+#'   !optional).
+#' @param ... additional arguments to be passed to or from methods.
+#' @param stringsAsFactors logical: should the character vector be converted to
+#'   a factor?
+#' @importFrom dplyr bind_cols
+as.data.frame.metadata <- function(x, row.names=NULL, optional=FALSE, ..., stringsAsFactors=FALSE) {
+  cust.df <- tryCatch({
+    custdf <- as.data.frame(slot(x, 'custom'))
+    if(is.data.frame(custdf) && nrow(custdf) == 1) custdf else stop()
+  }, error=function(e) NULL)
+  meta.df <- as.data.frame(attributes(x)[-which(names(attributes(x)) %in% c('custom','class'))], row.names=row.names, optional=optional, ..., stringsAsFactors=stringsAsFactors)
+  if(!is.null(cust.df)) {
+    meta.df <- bind_cols(meta.df, cust.df)
+  }
+  meta.df
+}
