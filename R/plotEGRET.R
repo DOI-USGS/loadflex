ebb8f6ae951b8a6f20f69b2abb7d83317ac98ced
Lindsay Carr <lcarr@usgs.gov>
2017-01-25 13:24:31 -0600

    override EGRET plotting default

--- R/plotEGRET.R
+++ R/plotEGRET.R
@@ -11,6 +11,8 @@
 #' flow, conc.units, site.id, and consti.name
 #' @param preds.type character specifying if the predictions being used are
 #' concentrations ("Conc") or fluxes ("Flux").
+#' @param moreTitle additional text to include in the fluxBiasMulti plot 
+#' title. The EGRET default is "WRTDS", so this changes the default to NULL.
 #' @param ... additional arguments to pass to the plot
 #'
 #' @details EGRET plots that require \code{fitdat, meta}:
@@ -91,7 +93,7 @@
 #' plotEGRET("multiPlotDataOverview", lamprey_nitrate, estdat, preds, meta)
 #' 
 plotEGRET <- function(plot.name, fitdat = NULL, estdat = NULL, preds = NULL, 
-                      meta = NULL, preds.type = "Conc", ...) {
+                      meta = NULL, preds.type = "Conc", moreTitle = NULL, ...) {
   
   req_missing <- switch(plot.name,
                         
@@ -149,7 +151,7 @@ plotEGRET <- function(plot.name, fitdat = NULL, estdat = NULL, preds = NULL,
          boxConcThree = boxConcThree(egretobj, ...), 
          plotConcHist = plotConcHist(egretobj, ...), 
          plotFluxHist = plotFluxHist(egretobj, ...),
-         fluxBiasMulti = fluxBiasMulti(egretobj, moreTitle = NULL, ...),
+         fluxBiasMulti = fluxBiasMulti(egretobj, moreTitle = moreTitle, ...),
          
          # default if no name matches
          stop(paste('unrecognized plot.name:', plot.name)))
