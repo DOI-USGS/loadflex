b4fb2293935a8bd4c250d03a5cdf8d51568f1804
Alison Appling <aappling@usgs.gov>
2017-07-25 15:21:38 -0700

    light edits to Lamprey data documentation

--- R/dataDocumentation.R
+++ R/dataDocumentation.R
@@ -1,20 +1,21 @@
 #' @name lamprey_discharge
-#' @title Discharge data for the Lamprey River. 
-#' @description Discharge data for the Lamprey River from 10/1/1999 to 11/16/2014.
-#' Discharge in CFS measured every  15 minutes and collected by the US Geological
-#'  Survey, site 1073500, waterdata.usgs.gov. The Lamprey River is an 81-km river 
-#' flowing through southeastern New Hampshire. Its 548-km2 watershed empties into 
-#' the Great Bay estuary.
+#' @title Discharge data for the Lamprey River.
+#' @description Discharge data for the Lamprey River from 10/1/1999 to
+#'   11/16/2014. Discharge in CFS measured every  15 minutes and collected by
+#'   the US Geological Survey, site 01073500, waterdata.usgs.gov. The Lamprey
+#'   River is an 81-km river flowing through southeastern New Hampshire. Its
+#'   548-km2 watershed empties into the Great Bay estuary.
 #' @docType data
 NULL
+
 #' @name lamprey_nitrate
-#' @title Nitrate data for the Lamprey River. 
+#' @title Nitrate data for the Lamprey River.
 #' @description Nitrate data for the Lamprey River from 10/1/1999 to 11/16/2014.
-#' Nitrate is in mg/L and is collected weekly. The Lamprey River is an 81-km river 
-#' flowing through southeastern New Hampshire. Its 548-km2 watershed empties into 
-#' the Great Bay estuary.
-#' Nitrate concentrations have been monitored with weekly and event-based grab 
-#' samples at Packers Falls on the Lamprey since 10 September 1999 and are
-#' measured by SmartChem discrete analyzer (Westco, Brookfield, CT).
+#'   Nitrate is in mg/L and is collected weekly. The Lamprey River is an 81-km
+#'   river flowing through southeastern New Hampshire. Its 548-km2 watershed
+#'   empties into the Great Bay estuary. Nitrate concentrations have been
+#'   monitored with weekly and event-based grab samples at Packers Falls on the
+#'   Lamprey since 10 September 1999 and are measured by SmartChem discrete
+#'   analyzer (Westco, Brookfield, CT).
 #' @docType data
 NULL
