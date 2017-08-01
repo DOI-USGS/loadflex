1fbd9b4822db7962adef36ba46fe470d21924aa6
Lindsay Carr <lcarr@usgs.gov>
2017-08-01 17:12:56 -0500

    get rid of indents

--- R/aaa_onLoad.R
+++ R/aaa_onLoad.R
@@ -17,14 +17,14 @@ pkg_env <- onBuild()
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("This information is preliminary or provisional and
-                        is subject to revision. It is being provided to meet
-                        the need for timely best science. The information
-                        has not received final approval by the U.S. Geological
-                        Survey (USGS) and is provided on the condition that
-                        neither the USGS nor the U.S. Government shall be held
-                        liable for any damages resulting from the authorized
-                        or unauthorized use of the information.
-                        
-                        Funding for loadflex expires summer 2018, 
-                        after which bugfixes & new features will be minimal")
+  is subject to revision. It is being provided to meet
+  the need for timely best science. The information
+  has not received final approval by the U.S. Geological
+  Survey (USGS) and is provided on the condition that
+  neither the USGS nor the U.S. Government shall be held
+  liable for any damages resulting from the authorized
+  or unauthorized use of the information.
+  
+  Funding for loadflex expires summer 2018, 
+  after which bugfixes & new features will be minimal")
 }
